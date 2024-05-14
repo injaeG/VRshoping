ff91cec831cda33bfc9219bbede52f3ac5325bf7
injaeG <164496745+injaeG@users.noreply.github.com>
2024-05-14 21:51:04 +0900

    publish

---
+++ Assets/Script/MenuController2.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController2 : MonoBehaviour
+{
+    public void StartBtn()
+    {
+        SceneManager.LoadScene("FitVR Female");
+    }
+}
